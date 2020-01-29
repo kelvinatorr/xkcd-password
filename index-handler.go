c6ae2b98aa0d44c9a3f1b8369ac935bfdd1e1329
Kelvin Dealca <dealca@fsi.io>
2020-01-28 16:47:18 -0800

    Add getting the number of words to generate from the query params

--- index-handler.go
+++ index-handler.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
 // IndexPage struct to fill in the index.html template
 type IndexPage struct {
 	GeneratedPassword string
+	NumberOfWords     int
 }
 
 // IndexHandler Handler for the / path
@@ -16,9 +18,18 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		FourZeroFourHandler(w, r, http.StatusNotFound)
 		return
 	}
-	// TODO: Get number of words from the GET params.
-	password := GeneratePassword(4)
-	indexPage := IndexPage{GeneratedPassword: password}
+	getParams := r.URL.Query()
+	numberOfWords := 4
+	if requestedNumberOfWordsSlice, ok := getParams["numberOfWords"]; ok {
+		requestedNumberOfWords, err := strconv.Atoi(requestedNumberOfWordsSlice[0])
+		checkAndLog(err)
+		if err == nil && requestedNumberOfWords >= 3 && requestedNumberOfWords <= 8 {
+			numberOfWords = requestedNumberOfWords
+		}
+	}
+	// Get number of words from the GET params.
+	password := GeneratePassword(numberOfWords)
+	indexPage := IndexPage{GeneratedPassword: password, NumberOfWords: numberOfWords}
 	t, _ := template.ParseFiles("templates/index.html")
 	t.Execute(w, indexPage)
 }
