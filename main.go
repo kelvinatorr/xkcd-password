41d0343f21fdc250eea5067f31655d6f3f9a1e8f
Kelvin Dealca <dealca@fsi.io>
2020-01-28 18:10:58 -0800

    Add static file handler

--- main.go
+++ main.go
@@ -9,6 +9,30 @@ import (
 	"strings"
 )
 
+type fileSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file if it exists in the directory. If path is a directory it checks if index.html exists
+// and returns nil, err if it does not.
+func (fs fileSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		// Check if index.html exists. If it does not return 404.
+		// Without this, the default behavior is to return a directory listing, which we don't want.
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+	return f, nil
+}
+
 func main() {
 	rawWordsPath := flag.String("raw-words-path", "/etc/xkcd-password/words-raw.txt",
 		"The path to a list of common english words. One on each line")
@@ -22,6 +46,9 @@ func main() {
 
 	log.Println("Starting webserver. Listening on port 8080")
 	http.HandleFunc("/", IndexHandler)
+	// Handle files in the static directory
+	fs := http.FileServer(fileSystem{http.Dir("static")})
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe("localhost:8080", nil)
 }
 
